f98efcec9efdce00a085b1fc20f14c96e2571128
Ritika <ritikakumari1302@gmail.com>
2018-08-18 23:04:02 +0530

    Testing printing

--- main.py
+++ main.py
@@ -25,14 +25,15 @@ def read_email_from_gmail():
 
 
         for i in range(latest_email_id,first_email_id, -1):
-            typ, email_data = mail.fetch(i, '(RFC822)' )
+            typ, data = mail.fetch(i, '(RFC822)' )
+            #print "success 1"
 
             for response_part in data:
                 if isinstance(response_part, tuple):
                     msg = email.message_from_string(response_part[1])
                     email_subject = msg['subject']
                     email_from = msg['from']
-                    print 'From : ' + email_from + '\n'
+                ''' print 'From : ' + email_from + '\n'
                     print 'Subject : ' + email_subject + '\n'
                     raw_email = email_data[0][1]
                     raw_email_string = raw_email.decode('utf-8')
@@ -40,9 +41,9 @@ def read_email_from_gmail():
                     for part in email_message.walk():
                         if part.get_content_type() == "text/plain":
                             body = part.get_payload(decode=TRUE)
-                            print body
+                            print "Success 2"
                         else:
-                            continue
+                            continue'''
 
     except:
         print "Error"
