73d7cc7972814bdf3bd0fe34cf802f4f34b0c4a9
Ritika <ritikakumari1302@gmail.com>
2018-08-18 21:39:49 +0530

    Building the body

--- main.py
+++ main.py
@@ -16,7 +16,7 @@ def read_email_from_gmail():
         mail.login(FROM_EMAIL,FROM_PWD)
         mail.select('inbox')
 
-        type, data = mail.search(None, 'ALL')
+        type, data = mail.search(None, '(SINCE "16-Aug-2018" BEFORE "17-Aug-2018")')
         mail_ids = data[0]
 
         id_list = mail_ids.split()   
@@ -25,7 +25,7 @@ def read_email_from_gmail():
 
 
         for i in range(latest_email_id,first_email_id, -1):
-            typ, data = mail.fetch(i, '(RFC822)' )
+            typ, email_data = mail.fetch(i, '(RFC822)' )
 
             for response_part in data:
                 if isinstance(response_part, tuple):
@@ -34,6 +34,15 @@ def read_email_from_gmail():
                     email_from = msg['from']
                     print 'From : ' + email_from + '\n'
                     print 'Subject : ' + email_subject + '\n'
+                    raw_email = email_data[0][1]
+                    raw_email_string = raw_email.decode('utf-8')
+                    email_message = email.message_from_string(raw_email_string)
+                    for part in email_message.walk():
+                        if part.get_content_type() == "text/plain":
+                            body = part.get_payload(decode=TRUE)
+                            print body
+                        else:
+                            continue
 
     except:
         print "Error"
